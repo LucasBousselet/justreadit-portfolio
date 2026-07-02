599f43982fe047add7d113a221cc1e1edf2e63d5
Bousselet Lucas <BousseletLucas@gmail.com>
2026-07-01 17:38:08 -0700

    Adding networking pieces to Terraform
    VPC, IGW, Route Table, subnet, SG

--- terraform-demo/infra/app/main.tf
+++ terraform-demo/infra/app/main.tf
@@ -14,6 +14,73 @@ provider "aws" {
   profile = "terraform-process" # Uses aws cli profile "terraform-process", which uses short-lived credentials
 }
 
+resource "aws_vpc" "justreadit_vpc" {
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Product = "justreadit"
+  }
+}
+
+resource "aws_internet_gateway" "vpc_igw" {
+  vpc_id = aws_vpc.justreadit_vpc.id
+
+  tags = {
+    Product = "justreadit"
+  }
+}
+
+resource "aws_route_table" "justreadit_route_table" {
+  vpc_id = aws_vpc.justreadit_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.vpc_igw.id
+  }
+
+  tags = {
+    Product = "justreadit"
+  }
+}
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id     = aws_vpc.justreadit_vpc.id
+  cidr_block = "10.0.0.0/28"
+
+  tags = {
+    Product = "justreadit"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_1_rt_association" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.justreadit_route_table.id
+}
+
+resource "aws_security_group" "sg_allow_https" {
+  name        = "sg_allow_https"
+  description = "Allow HTTPS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.justreadit_vpc.id
+
+  tags = {
+    Product = "justreadit"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https_ipv4" {
+  security_group_id = aws_security_group.sg_allow_https.id
+  cidr_ipv4         = aws_vpc.justreadit_vpc.cidr_block
+  from_port         = 7070 # 443 once ALB is in place
+  ip_protocol       = "tcp"
+  to_port           = 7070 # 443 once ALB is in place
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound_ipv4" {
+  security_group_id = aws_security_group.sg_allow_https.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
 resource "aws_ecs_cluster" "justreadit_cluster" {
   name = "justreadit-cluster"
 
@@ -55,12 +122,12 @@ resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
 }
 
 resource "aws_ecs_task_definition" "justreadit_task_definition" {
-  family = "service"
+  family                   = "service"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
   cpu                      = 256
   memory                   = 512
-  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
   container_definitions = jsonencode([
     {
       name      = "justreadit-api"
@@ -86,6 +153,13 @@ resource "aws_ecs_service" "ecs_service" {
   cluster         = aws_ecs_cluster.justreadit_cluster.id
   task_definition = aws_ecs_task_definition.justreadit_task_definition.arn
   desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = [aws_subnet.public_subnet_1.id]
+    security_groups  = [aws_security_group.sg_allow_https.id]
+    assign_public_ip = true
+  }
 
   depends_on = [
     aws_iam_role_policy_attachment.ecs_execution_role_policy
