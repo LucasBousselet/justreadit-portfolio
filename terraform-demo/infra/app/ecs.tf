1b67082939f3dc54b2aa2eb3c55c14bbb3fa723a
Bousselet Lucas <BousseletLucas@gmail.com>
2026-07-05 18:56:05 -0700

    Fix missing health check
    Fixed missing health check that caused ECS task to never become healthy, and got terminated over and over again by the ECS service.

--- terraform-demo/infra/app/ecs.tf
+++ terraform-demo/infra/app/ecs.tf
@@ -30,6 +30,14 @@ resource "aws_ecs_task_definition" "justreadit_task_definition" {
         }
       ]
 
+      healthCheck = {
+        command     = ["CMD-SHELL", "curl -f http://localhost:7070/status || exit 1"]
+        interval    = 30
+        timeout     = 5
+        retries     = 3
+        startPeriod = 60
+      }
+
       logConfiguration = {
         logDriver = "awslogs"
 
