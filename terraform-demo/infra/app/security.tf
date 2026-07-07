0234bf093c795150cfb702498e8d1efbf3dd3d5d
Bousselet Lucas <BousseletLucas@gmail.com>
2026-07-06 20:33:54 -0700

    Add RDS Postgres + SG

--- terraform-demo/infra/app/security.tf
+++ terraform-demo/infra/app/security.tf
@@ -41,3 +41,25 @@ resource "aws_vpc_security_group_egress_rule" "allow_ecs_all_outbound_ipv4" {
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
+
+resource "aws_security_group" "sg_rds" {
+  name        = "${local.name}-sg-rds"
+  description = "Allow Postgres inbound traffic from ECS SG and all outbound traffic"
+  vpc_id      = aws_vpc.justreadit_vpc.id
+
+  tags = local.tags
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_postgres_inbound_ipv4" {
+  security_group_id            = aws_security_group.sg_rds.id
+  referenced_security_group_id = aws_security_group.sg_ecs_task.id # Traffic allowed only from ECS SG
+  from_port                    = 5432
+  ip_protocol                  = "tcp"
+  to_port                      = 5432
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_rds_all_outbound_ipv4" {
+  security_group_id = aws_security_group.sg_rds.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
---
+++ terraform-demo/infra/app/storage.tf
@@ -0,0 +1,31 @@
+resource "aws_db_instance" "justreadit_postgres_db" {
+  allocated_storage           = 20
+  max_allocated_storage       = 100 # Enables autoscaling because > allocated_storage 
+  db_name                     = "justreadit_db"
+  engine                      = "postgres"
+  engine_version              = "16"
+  instance_class              = "db.t4g.micro"
+  username                    = "admin"
+  manage_master_user_password = true # Automatically creates an entry in Secrets Manager
+  skip_final_snapshot         = true # For demo app only
+  db_subnet_group_name        = aws_db_subnet_group.postgres_subnet_group.name
+  publicly_accessible         = false
+  storage_encrypted           = true
+  deletion_protection         = false
+  backup_retention_period     = 7 # Enables automated backups
+
+  vpc_security_group_ids = [
+    aws_security_group.sg_rds.id
+  ]
+}
+
+resource "aws_db_subnet_group" "postgres_subnet_group" {
+  name = "${local.name}-postgres-subnet-group"
+
+  subnet_ids = [
+    aws_subnet.private_subnet_1.id,
+    aws_subnet.private_subnet_1.id
+  ]
+
+  tags = local.tags
+}
